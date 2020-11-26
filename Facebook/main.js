e8999404eee7521be17afb563f2e43817eac0bf0
TaykoWS <54807648+TaykoWS@users.noreply.github.com>
2020-11-26 17:48:15 +0100

    yolo bis

--- Facebook/main.js
+++ Facebook/main.js
@@ -1,3 +1,39 @@
-import {News} from "./news";
-import {Nav} from "./nav";
-import {Messenger} from "./messenger";
+//import {News} from "./news";
+//import {Nav} from "./nav";
+//import {Messenger} from "./messenger";
+
+//MODULE 1//-----------------------------------
+$("button").click(function(e){
+    e.preventDefault();
+    const message = $("form").serializeArray();
+    $.ajax({
+        url: "init.php?function=postMessage",
+        method:"POST",
+        data:message,
+        success:getMessages()
+    })
+})
+
+function getMessages(){
+    $("#affichage").html("");
+    
+    $.ajax({
+        url: "init.php",
+        dataType: "JSON",
+        data:{
+            function:"getMessages"
+        },
+
+        success:function(response){
+            response.forEach(message => {
+                $("#affichage").append(`
+                <p>${message.text}</p>
+                `)
+            });
+        }
+    })
+}
+
+//MODULE 2//-----------------------------------
+
+
---
+++ Facebook/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+$pdo = new PDO("mysql:host=localhost:3306;dbname=fbynovjs", 'root', '');
+
+switch ($_GET["function"]) {
+    case 'postMessage':
+        $prepare = $pdo->prepare("INSERT INTO status (text) 
+                                    VALUES (:text)");
+        $prepare->execute($_POST);
+
+        echo "Message sauvegardé";
+        break;
+        
+        case 'getMessages':
+
+            $query = $pdo->query("SELECT * FROM status ORDER BY id_text");
+
+            echo json_encode($query->fetchAll(PDO::FETCH_ASSOC));
+            break;
+    }
